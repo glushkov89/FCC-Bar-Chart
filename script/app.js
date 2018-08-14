8a245e04baef401b26f460b3c410c1f7c8e35939
glushkov89 <kellerman117@gmail.com>
2018-08-14 11:48:19 +0300

    Browserify and watchify configured.

--- script/app.js
+++ script/app.js
@@ -1,41 +1,6 @@
 const appMod = (function() {
-	const ids = {
-		preview: "#preview",
-		editor: "#editor"
-	};
-
-	const myMarked = require("marked");
-	let newRenderer = new myMarked.Renderer();
-	newRenderer.link = function(href, title, text) {
-		return `<a title=${title} href="${href}" target="_blank">${text}</a>`;
-	};
-
-	const myMarkedSettings = {
-		gfm: true,
-		breaks: true,
-		renderer: newRenderer
-	};
-
-	const autosize = require("autosize");
-
-	return {
-		convertText: function() {
-			$(ids.preview).html(myMarked($(ids.editor).val(), myMarkedSettings));
-		},
-		attachEventHandlers: function() {
-			$(ids.editor).on("input", this.convertText);
-			$(ids.preview).on("click", () => {
-				$(ids.preview).toggleClass("down");
-			});
-		},
-		enableAutosize: function() {
-			autosize($(ids.editor));
-		}
-	};
+	const d3 = require("d3");
+	return {};
 })();
 
-$(document).ready(() => {
-	appMod.enableAutosize();
-	appMod.convertText();
-	appMod.attachEventHandlers();
-});
+$(document).ready(() => {});
